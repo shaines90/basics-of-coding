cc3a2e977884f0d2c19b50cbc15bca39c518129c
Sophia Haines <sophia.haines@gmail.com>
2014-07-16 15:06:56 +0800

    updating user.rb for image uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :profile_img, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+  validates_attachment_content_type :profile_img, :content_type => /\Aimage\/.*\Z/
   validates :first_name, presence:true
   validates :last_name, presence:true
   validates :bio, presence:true
   validates :phone, presence:true
   validates :username, presence:true
-end
 
+end
